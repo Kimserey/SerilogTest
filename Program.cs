4a7cdf55f524012b99d9c8f6655f410db735fdba
Kimserey <kimserey.lam@groomgy.com>
2018-02-12 13:42:00 +0800

    serilog

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Microsoft.Extensions.Configuration;
+using Serilog;
 
 namespace SerilogTest
 {
@@ -6,6 +8,17 @@ namespace SerilogTest
     {
         static void Main(string[] args)
         {
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .WriteTo.File("log.txt", rollingInterval: RollingInterval.Day, retainedFileCountLimit: 3)
+                .CreateLogger();
+
+            logger.Information("Hello, world!");
+            
             Console.WriteLine("Hello World!");
         }
     }
