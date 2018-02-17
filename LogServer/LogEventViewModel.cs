e796af36ddea9ccb0c803b9e5ff0e85f3fcc1a36
Kimserey <kimserey.lam@groomgy.com>
2018-02-17 15:50:31 +0800

    more

--- LogServer/LogEventViewModel.cs
+++ LogServer/LogEventViewModel.cs
@@ -9,6 +9,7 @@ namespace LogServer
         public string Level { get; set; }
         public string RenderedMessage { get; set; }
         public string MessageTemplate { get; set; }
+        public string Exception { get; set; }
         public Dictionary<string, object> Properties { get; set; }
     }
 }
